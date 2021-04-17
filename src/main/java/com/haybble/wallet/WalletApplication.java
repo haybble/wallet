48ae8d15f43c775282b3726443dde31f083f5121
haybble <fhenry007@gmail.com>
2021-04-17 22:42:54 +0100

    heruko config

--- src/main/java/com/haybble/wallet/WalletApplication.java
+++ src/main/java/com/haybble/wallet/WalletApplication.java
@@ -13,9 +13,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class WalletApplication {
+    
+    
+      @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
     @Autowired
     UserRepository userRepository;
